b82c79737ee75a89be0a8106676e14a15207fb64
Rahmana Muhammad <rahmanapyrr@fb.com>
2018-07-02 10:24:43 -0700

    initial commit

---
+++ app/src/main/java/e/rahmanapyrr/twitter/Twitter.java
@@ -0,0 +1,13 @@
+package e.rahmanapyrr.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Twitter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter);
+    }
+}
